3667d19b10005353a5f01c2a845b5bb9f7a044d8
Raymond <raymondtykong@gmail.com>
2023-09-30 00:57:08 -0700

    adding post and get functions

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,3 +1,27 @@
 const express = require('express')
+const {
+    createWorkout,
+    getWorkout,
+    getWorkouts
+} = require("../controllers/workoutControllers")
 
 const router = express.Router()
+
+router.get('/', getWorkouts)
+
+router.get('/:id',getWorkout)
+
+//POST new workout
+router.post('/', createWorkout)
+
+//DELETE new workout
+router.delete('/:id', (req,res) => {
+    res.json({msg: 'delete new workout'})
+})
+
+//UPDATE workout
+router.patch('/:id', (req,res) => {
+    res.json({msg: 'update new workout'})
+})
+
+module.exports = router
